a7d2035dec447840a9dfd78253916ae5775e1a81
savaliyabhargav <savaliyabhargav644@gmail.com>
2026-02-06 09:58:43 +0530

    UserService compleated

--- UserService/UserService/src/main/java/com/example/UserService/dto/UserResponse.java
+++ UserService/UserService/src/main/java/com/example/UserService/dto/UserResponse.java
@@ -10,11 +10,11 @@ import java.time.LocalDateTime;
 @Data
 public class UserResponse {
     private String id;
+    private String keycloakId;
     private String email;
-    private String passward ;
+    private String password;
     private String firstName;
     private String lastName;
-    private UserRole role = UserRole.USER;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
