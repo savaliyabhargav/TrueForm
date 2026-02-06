e9d0b7b5310291c49f57a43290505564d1e049aa
savaliyabhargav <savaliyabhargav644@gmail.com>
2026-02-06 09:57:16 +0530

    RegisterRequest compleated

--- UserService/UserService/src/main/java/com/example/UserService/dto/RegisterRequest.java
+++ UserService/UserService/src/main/java/com/example/UserService/dto/RegisterRequest.java
@@ -1,14 +1,25 @@
 package com.example.UserService.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 
+@Data
 public class RegisterRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must have atleast 6 characters")
     private String password;
+
+    private String keycloakId;
+
     private String firstName;
     private String lastName;
-
 
 }
